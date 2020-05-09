3875852c3d51b014ce45d7fa3704641ddfd3f918
Bhushan C. Mhatre <44663470+thebhushanmhatre@users.noreply.github.com>
2020-05-10 01:07:41 +0530

    Displaying Instructions and Ingredients

--- src/App.js
+++ src/App.js
@@ -6,28 +6,79 @@ import InputForm from './components/InputForm'
 import DisplayItems from './components/DisplayItems'
 import PopularItems from './components/PopularItems'
 
+const example = {
+  dateModified: null,
+  idMeal: "52865",
+  strArea: "Indian",
+  strCategory: "Vegetarian",
+  strDrinkAlternate: null,
+  strIngredient1: "Sunflower Oil",
+  strIngredient2: "Paneer",
+  strIngredient3: "Ginger",
+  strIngredient4: "Cumin",
+  strIngredient5: "Turmeric",
+  strIngredient6: "Coriander",
+  strIngredient7: "Green Chilli",
+  strIngredient8: "Tomato",
+  strIngredient9: "Peas",
+  strIngredient10: "Garam Masala",
+  strIngredient11: "Coriander",
+  strIngredient12: "Naan Bread",
+  strIngredient13: "",
+  strIngredient14: "",
+  strIngredient15: "",
+  strIngredient16: "",
+  strIngredient17: "",
+  strIngredient18: "",
+  strIngredient19: "",
+  strIngredient20: "",
+  strInstructions: "Heat the oil in a frying pan over high heat until it’s shimmering hot. Add the paneer, then turn the heat down a little. Fry until it starts to brown at the edges, then turn it over and brown on each side – the paneer will brown faster than you think, so don’t walk away. Remove the paneer from the pan and drain on kitchen paper. Put the ginger, cumin, turmeric, ground coriander and chilli in the pan, and fry everything for 1 min. Add the tomatoes, mashing them with the back of a spoon and simmer everything for 5 mins until the sauce smells fragrant. Add a splash of water if it’s too thick. Season well. Add the peas and simmer for a further 2 mins, then stir in the paneer and sprinkle over the garam masala. Divide between two bowls, top with coriander leaves and serve with naan bread, roti or rice.",
+  strMeal: "Matar Paneer",
+  strMealThumb: "https://www.themealdb.com/images/media/meals/xxpqsy1511452222.jpg",
+  strMeasure1: "1 tbls",
+  strMeasure2: "225g",
+  strMeasure3: "2",
+  strMeasure4: "1 tsp ",
+  strMeasure5: "1 tsp ",
+  strMeasure6: "1 tsp ",
+  strMeasure7: "1",
+  strMeasure8: "4 large",
+  strMeasure9: "150g",
+  strMeasure10: "1 tsp ",
+  strMeasure11: "Small bunch",
+  strMeasure12: "to serve",
+  strMeasure13: "",
+  strMeasure14: "",
+  strMeasure15: "",
+  strMeasure16: "",
+  strMeasure17: "",
+  strMeasure18: "",
+  strMeasure19: "",
+  strMeasure20: "",
+  strSource: "https://www.bbcgoodfood.com/recipes/matar-paneer",
+  strTags: "Curry,Vegetarian",
+  strYoutube: "https://www.youtube.com/watch?v=wlseYNqwLNs"
+}
 export default function App() {
-  const [data, setData] = useState({meals: []})
+  const [data, setData] = useState({meals: [example]})  
   
   const getDish = async() => {
     let receipename = document.getElementById('receipename').value
-    let url = "https://www.themealdb.com/api/json/v1/1/search.php?s="+receipename
-    console.log("Inside getDish", receipename)
-    console.log("Inside getDish", url)
-    let response = await fetch(url)
-    let mealData = await response.json()
-    console.log('data', mealData)
-    if (mealData.meals != null){
-      setData(mealData)
-    }
-    else if (mealData.meals==null) {
-      let msg = 'No Dishes found with name: "'+receipename+'"'
-      setData({meals: [], message: msg})
+    if(receipename != null && receipename.length > 0){
+      let url = "https://www.themealdb.com/api/json/v1/1/search.php?s="+receipename
+      let response = await fetch(url)
+      let mealData = await response.json()
+      if (mealData.meals != null){
+        setData(mealData)
+      }
+      else if (mealData.meals==null) {
+        let msg = 'No Dishes found with name: "'+receipename+'"'
+        setData({meals: [], message: msg})
+      }
     }
   }
 
   const getPopularDish = async(receipename) => {
-    console.log("Inside getPopularDish", receipename)
     let url = "https://www.themealdb.com/api/json/v1/1/search.php?s="+receipename
     let response = await fetch(url)
     let mealData = await response.json()
@@ -38,7 +89,7 @@ export default function App() {
     <Container>
       <InputForm getDish={getDish} />
       <DisplayItems items={data} />
-      {(data.meals==null || !data.meals.length>0) &&
+      {(data.meals==null || data.meals.length<4) &&
         <PopularItems getPopularDish={getPopularDish} />
       }
       <Footer />
--- src/components/DisplayItems.js
+++ src/components/DisplayItems.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Card, Collapse, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 export default function DisplayItems(props){
-  var itemList = props.items.meals.map(item => {
-    return (<li key={item.idMeal}>{item.strMeal}</li>)
+  var itemList = props.items.meals.map(i => {
+    return <RenderItem item={i} />
   })
-  
-  return (
+
+  return(
     <div>
-      <ul>{itemList}</ul>
+      <div>{itemList}</div>
       <p style={{fontSize:20}}>{props.items.message}</p>
     </div>
   )
+}
+
+// Single Element
+function RenderItem({item}){
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  const getElements = (item) => {
+    let nums = Array.from(Array(20).keys())
+    let elements = []
+    nums.map(i => elements.push(item['strIngredient'+ (i+1)]))
+    return [...elements.filter(x=>x)].join(', ')
+  }
+
+  const getSteps = (item) => {
+    let steps = item.strInstructions.split('.')
+    return [...steps.filter(x=>x)].map(p=><li>{p}.</li>)
+  }
+
+  return (
+    <Card style={{margin: 10}}>
+      <CardBody>
+        <CardTitle>{item.strMeal}</CardTitle>
+        <CardSubtitle>{item.strTags}</CardSubtitle>
+      <Button color="primary" onClick={toggle} style={{ marginTop: 10, marginBottom: 10}}>Receipe</Button>
+        <Collapse isOpen={isOpen}>
+          <CardText>Ingredients: {getElements(item)}</CardText>
+          <CardText>Instructions: <ol>{getSteps(item)}</ol></CardText>
+        </Collapse>
+      </CardBody>
+    </Card>
+  )
+
 }
